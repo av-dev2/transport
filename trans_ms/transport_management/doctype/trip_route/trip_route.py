a6274e6d7424ae67b80176726f3070a5b48acb57
Aakvatech Limited <info@aakvatech.com>
2022-03-23 07:44:13 +0100

    fix: allow case senstive trip route type

--- trans_ms/transport_management/doctype/trip_route/trip_route.py
+++ trans_ms/transport_management/doctype/trip_route/trip_route.py
@@ -4,6 +4,7 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from telegram import Location
 
 
 class TripRoute(Document):
@@ -12,12 +13,12 @@ class TripRoute(Document):
 
     def before_save(self):
         for d in self.get("trip_steps"):
-            if d.idx == 1 and d.location_type.lower != "loading point":
+            if d.idx == 1 and d.location_type.lower() != "loading point":
                 frappe.throw("Set 1st location type to LOADING POINT")
                 break
             if (
                 d.idx == len(self.get("trip_steps"))
-                and d.location_type.lower != "offloading point"
+                and d.location_type.lower() != "offloading point"
             ):
                 frappe.throw("Set last location type to OFFLOADING POINT")
                 break
