e4a4be69e2bf2218bbc192118998aef941cd544f
aakvatech <info@aakvatech.com>
2023-02-24 10:06:10 +0000

    feat: Trip number in invoice and Vehicle trip date

--- trans_ms/transport_management/doctype/transportation_order/transportation_order.py
+++ trans_ms/transport_management/doctype/transportation_order/transportation_order.py
@@ -336,6 +336,8 @@ def create_sales_invoice(doc, rows):
             description += "<b>VEHICLE NUMBER: " + row["vehicle_plate_number"]
         if row["route"]:
             description += "<BR>ROUTE: " + row["route"]
+        if row["created_trip"]:
+            description += "<BR>TRIP: " + row["created_trip"]
         item = frappe._dict({
                 "item_code": row["item"],
                 "qty": 1,
